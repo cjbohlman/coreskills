1ef4d21f1d8c057b4c5bf2e434390fa9c51f31df
Chris Bohlman <cbohlman@pm.me>
2025-06-22 19:36:55 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     exclude: ['lucide-react'],
   },
   server: {
+    historyApiFallback: true,
     headers: {
       'Content-Security-Policy': '',
       'X-Frame-Options': 'SAMEORIGIN'
